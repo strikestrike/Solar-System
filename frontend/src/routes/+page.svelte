98cca3dc0f557ba710f5213f3e4c335965ad23fd
Baby Pooh <babybearbear1280@gmail.com>
2023-01-23 03:24:44 -0500

    clear home page(frontend)

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -13,65 +13,9 @@
 </script>
 
 <svelte:head>
-	<title>Conduit</title>
+	<title>ProfitFlow</title>
 </svelte:head>
 
 <div class="home-page">
-	{#if !data.user}
-		<div class="banner">
-			<div class="container">
-				<h1 class="logo-font">conduit</h1>
-				<p>A place to share your knowledge.</p>
-			</div>
-		</div>
-	{/if}
-
-	<div class="container page">
-		<div class="row">
-			<div class="col-md-9">
-				<div class="feed-toggle">
-					<ul class="nav nav-pills outline-active">
-						<li class="nav-item">
-							<a href="/?tab=all" class="nav-link" class:active={tab === 'all' && !tag}>
-								Global Feed
-							</a>
-						</li>
-
-						{#if data.user}
-							<li class="nav-item">
-								<a href="/?tab=feed" class="nav-link" class:active={tab === 'feed'}>Your Feed</a>
-							</li>
-						{:else}
-							<li class="nav-item">
-								<a href="/login" class="nav-link">Sign in to see your Feed</a>
-							</li>
-						{/if}
-
-						{#if tag}
-							<li class="nav-item">
-								<a href="/?tag={tag}" class="nav-link active">
-									<i class="ion-pound" />
-									{tag}
-								</a>
-							</li>
-						{/if}
-					</ul>
-				</div>
-
-				<ArticleList articles={data.articles} />
-				<Pagination pages={data.pages} {p} href={(p) => `/?${page_link_base}&page=${p}`} />
-			</div>
-
-			<div class="col-md-3">
-				<div class="sidebar">
-					<p>Popular Tags</p>
-					<div class="tag-list">
-						{#each data.tags as tag}
-							<a href="/?tag={tag}" class="tag-default tag-pill">{tag}</a>
-						{/each}
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+	<h1>Welcome!</h1>
 </div>
