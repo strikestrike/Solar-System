3314db03f0c8bb8175a97a2b2bc7f0bb826c1047
Baby Pooh <babybearbear1280@gmail.com>
2023-02-05 14:46:50 -0500

    some fix

--- frontend/src/routes/login.js
+++ frontend/src/routes/login.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 const headers = {
     "Content-Type": "application/json",
     Accept: "application/json",
@@ -9,24 +11,19 @@ export async function post(req, res) {
 
         const {BACKEND_HOST} = process.env;
 
-        const result = await fetch(BACKEND_HOST + `/api/signin`, {
-            method: "POST",
-            headers,
-            body: JSON.stringify({ email, password }),
-        });
+        await axios.post(BACKEND_HOST + '/api/signin', {email, password})
+            .then(response => {
+                // handle success
+                let user = response.data.user;
 
-        const parsed = await result.json();
+                req.session.token = user;
+                req.session.role = user.role;
 
-        if (typeof parsed.error !== "undefined") {
-            throw new Error(parsed.error);
-        }
-
-        let user = parsed.user;
-
-        req.session.token = user;
-        req.session.role = user.role;
-
-        res.end(JSON.stringify({ token: user, role: user.role }));
+                res.end(JSON.stringify({ token: user, role: user.role }));
+            })
+            .catch(error => {
+                throw new Error(error.response.data.error);
+            });
     } catch (error) {
         res.end(JSON.stringify({ error: error.message }));
     }
